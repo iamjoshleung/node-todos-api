126efaa53e2f8407210b2aee3ca81c8e6d9e3161
joshuajazleung <joshuajazleung@gmail.com>
2016-12-11 16:11:29 +0800

    Add POST /todos and refactored server folders

---
+++ server/models/todo.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose')
+
+const Todo = mongoose.model('Todos', {
+  text: {
+    type: String,
+    required: true,
+    trim: true,
+    minlength: 1
+  },
+  completed: {
+    type: Boolean,
+    default: false
+  },
+  completedAt: {
+    type: Number,
+    default: null
+  }
+})
+
+module.exports = {Todo}
