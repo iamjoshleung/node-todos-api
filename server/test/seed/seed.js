3802cb439adb9ae80e8d4db390220ffd98438513
joshuajazleung <joshuajazleung@gmail.com>
2016-12-19 18:50:13 +0800

    add seed data and test POST /users/ and GET /usrs/me

---
+++ server/test/seed/seed.js
@@ -0,0 +1,49 @@
+const {ObjectID} = require('mongodb')
+const jwt = require('jsonwebtoken')
+
+const {User} = require('./../../models/user')
+const {Todo} = require('./../../models/todo')
+
+const userOneId = new ObjectID()
+const userTwoId = new ObjectID()
+
+const users = [{
+  _id: userOneId,
+  email: 'joshuajazleung@gmail.com',
+  password: '123456Pass',
+  tokens: [{
+    access: 'auth',
+    token: jwt.sign({_id: userOneId, access: 'auth'}, 'secretSalt').toString()
+  }]
+}, {
+  _id: userTwoId,
+  email: 'joshualeung1830@gmail.com',
+  password: '123456Pass'
+}]
+
+const populateUsers = (done) => {
+  User.remove({}).then(() => {
+    let userOne = new User(users[0]).save()
+    let userTwo = new User(users[1]).save()
+
+    return Promise.all([userOne, userTwo])
+  }).then(() => done())
+}
+
+const text = [{
+  _id: new ObjectID(),
+  text: 'Text one'
+}, {
+  _id: new ObjectID(),
+  text: 'Text two',
+  completed: true,
+  completedAt: 333,
+}]
+
+const populateTodos = (done) => {
+  Todo.remove({}).then(() => {
+    return Todo.insertMany(text)
+  }).then(() => done())
+}
+
+module.exports = {users, text, populateTodos, populateUsers}
--- server/test/server.test.js
+++ server/test/server.test.js
@@ -4,22 +4,12 @@ const {ObjectID} = require('mongodb')
 
 const {app} = require('./../server')
 const {Todo} = require('./../models/todo')
+const {User} = require('./../models/user')
 
-let text = [{
-  _id: new ObjectID(),
-  text: 'Text one'
-}, {
-  _id: new ObjectID(),
-  text: 'Text two',
-  completed: true,
-  completedAt: 333,
-}]
+const {text, users, populateUsers, populateTodos} = require('./seed/seed')
 
-beforeEach((done) => {
-  Todo.remove({}).then(() => {
-    return Todo.insertMany(text)
-  }).then(() => done())
-})
+beforeEach(populateUsers)
+beforeEach(populateTodos)
 
 describe('GET /todos', () => {
   it('should get all todos', (done) => {
@@ -135,6 +125,81 @@ describe('PATCH /todos/:id', () => {
         expect(res.body.doc.completed).toBe(false)
         expect(res.body.doc.completedAt).toNotExist()
       })
+      .end(done)
+  })
+})
+
+describe('GET /users/me', () => {
+  it('should return user if authenticated', (done) => {
+    request(app)
+      .get('/users/me')
+      .set('x-auth', users[0].tokens[0].token)
+      .expect(200)
+      .expect((res) => {
+        expect(res.body._id).toBe(users[0]._id.toHexString())
+        expect(res.body.email).toBe(users[0].email)
+      })
+      .end(done)
+  })
+
+  it('should return 401 if not authenticated', (done) => {
+    request(app)
+      .get('/users/me')
+      .expect(401)
+      .expect((res) => {
+        expect(res.body).toEqual({})
+      })
+      .end(done)
+  })
+})
+
+describe('POST /users', () => {
+  it('should create a user', (done) => {
+    let email = 'joshuajazleung213112@gmail.com'
+    let password = '123456Pass'
+
+    request(app)
+      .post('/users')
+      .send({email, password})
+      .expect(200)
+      .expect((res) => {
+        expect(res.headers['x-auth']).toExist()
+        expect(res.body.email).toBe(email)
+        expect(res.body._id).toExist()
+      })
+      .end((err) => {
+        if (err) return done(err)
+
+        User.findOne({email}).then((user) => {
+          expect(user).toExist()
+          expect(user.password).toNotBe(password)
+          done()
+        }).catch((e) => done(e))
+      })
+  })
+
+  it('should return validation errors if request invalid', (done) => {
+    let email = 'dgsagre'
+    let password = '222'
+
+    request(app)
+      .post('/users')
+      .send({email, password})
+      .expect(400)
+      .expect((res) => {
+        expect(res.headers['x-auth']).toNotExist()
+      })
+      .end(done)
+  })
+
+  it('should not create user if email in user', (done) => {
+    request(app)
+      .post('/users')
+      .send({
+        email: users[0].email,
+        password: '123456Pass'
+      })
+      .expect(400)
       .end(done)
   })
 })
